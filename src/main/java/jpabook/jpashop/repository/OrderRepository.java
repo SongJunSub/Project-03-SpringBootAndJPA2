4cea81e4d36686c09752e49e41ba813a701b2a9d
SongJunSub <s911130@naver.com>
2024-02-19 16:06:37 +0900

    프로젝트 완료 후 Commit & Push

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,17 +1,29 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.OrderStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
+
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
-    public final EntityManager em;
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em){
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order){
         em.persist(order);
@@ -21,14 +33,105 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
     public List<Order> findAll(OrderSearch orderSearch){
 
-        return em.createQuery("SELECT o FROM Order o JOIN o.member m" +
-                        " WHERE o.status = :status" +
-                        " AND m.name LIKE :name", Order.class)
-                .setParameter("status", orderSearch.getOrderStatus())
-                .setParameter("name", orderSearch.getMemberName())
-                .setMaxResults(1000) //최대 1000건
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+
+    }
+
+    private BooleanExpression nameLike(String memberName){
+
+        if(!StringUtils.hasText(memberName)){
+            return null;
+        }
+
+        return member.name.like(memberName);
+
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond){
+
+        if(statusCond == null){
+            return null;
+        }
+
+        return order.status.eq(statusCond);
+
+    }
+
+    public List<Order> findAllWithMemberDelivery(){
+
+        return em.createQuery("SELECT o FROM Order o" +
+                " JOIN FETCH o.member m" +
+                " JOIN FETCH o.delivery d", Order.class
+        ).getResultList();
+
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit){
+
+        return em.createQuery("SELECT o FROM Order o" +
+                " JOIN FETCH o.member m" +
+                " JOIN FETCH o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+
+    }
+
+    public List<Order> findAllWithItem(){
+
+        //1대다 쿼리는 페이징 불가능
+        return em.createQuery(
+                "SELECT DISTINCT o FROM Order o" +
+                " JOIN FETCH o.member m" +
+                " JOIN FETCH o.delivery d" +
+                " JOIN FETCH o.orderItems oi" +
+                " JOIN FETCH oi.item i", Order.class)
+                //.setFirstResult(1)
+                //.setMaxResults(100)
                 .getResultList();
 
     }
--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -39,7 +39,16 @@ public class MemberService {
 
     //회원 단건 조회
     public Member findOne(Long memberId){
-        return memberRepository.findOne(memberId);
+        return memberRepository.findById(memberId).get();
+    }
+
+    @Transactional
+    public void update(Long id, String name){
+
+        Member member = memberRepository.findById(id).get();
+
+        member.setName(name);
+
     }
 
 }
